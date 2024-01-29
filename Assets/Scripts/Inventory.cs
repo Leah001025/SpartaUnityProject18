843d2af753f94810ef935e1b98a4415d11017c54
ruiroll <hyunbean67@gmail.com>
2024-01-29 17:34:48 +0900

    [add]240129_item
    add inventory, slots, and item

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,15 +1,115 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class Inventory : MonoBehaviour //Inventory
 {
+    public static bool inventoryActivated = false;
     public List<Item> items;
 
+    [SerializeField] private GameObject bag;
     [SerializeField] private Transform slotParent;
     [SerializeField] private Slot[] slots;
 
-    private void OnValidate() {
-        slots = slotParent.GetComponentsInChildren<Slot>();    
+    private void OnValidate() // change slots if changed by editor
+    {
+        slots = slotParent.GetComponentsInChildren<Slot>();
     }
+
+    void Awake() 
+    {
+        FreshSlot();
+    }
+
+    void Update()
+    {
+        TryOpenInventory();
+    }
+
+    private void TryOpenInventory()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryActivated = !inventoryActivated;
+
+            if(inventoryActivated) OpenInventory();
+            else CloseInventory();
+        }
+    }
+
+    private void OpenInventory()
+    {
+        bag.SetActive(true);
+    }
+
+    private void CloseInventory()
+    {
+        bag.SetActive(false);
+    }
+
+    public void FreshSlot() // reload slots & show items
+    {
+        int i = 0;
+        for(; i < items.Count && i < slots.Length; i++)
+        {
+            slots[i].item = items[i];
+        }
+        for(; i < slots.Length; i++)
+        {
+            slots[i].item = null;
+        }
+    }
+
+    public void AddItem(Item _item)
+    {
+        Debug.Log("실행");
+        if(items.Count < slots.Length)
+        {
+            items.Add(_item);
+            FreshSlot();
+        }
+        else
+        {
+            Debug.Log("슬롯이 가득 차 있습니다.");
+        }
+    }
+
+    public void RemoveItem(Item _item)
+    {
+        items.Remove(_item);
+        FreshSlot();
+    }
+
+
+    public void UseItem(Item item)
+    {
+        if(item.name == "CurryRice")
+        {
+            Debug.Log("체력 1 회복");
+            // hp 회복
+        }
+        else if(item.name == "Scateboard")
+        {
+            Debug.Log("이동속도 증가");
+            // 이속증가
+        }
+        else if(item.name == "LemonPie")
+        {
+            Debug.Log("레몬파이사용");
+            // hp 회복
+        }
+        else if(item.name == "RunningShoes")
+        {
+            Debug.Log("운동화사용");
+            // 데미지 증가
+        }
+        else 
+        {
+            Debug.Log("NullItemException");
+        }
+        RemoveItem(item);
+    }
+
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+// Item Type 추가
 
 [CreateAssetMenu]
 public class Item : ScriptableObject
 {
     public string itemName;
+    public string itemDetail;
     public Sprite itemImage;
 }
