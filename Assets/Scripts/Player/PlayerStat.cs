c7a4eab7da42c04894e67b632ea6f10708ba5ff2
chai227chai <chai227chai@naver.com>
2024-01-30 20:54:30 +0900

    [Fix]240130_Combine

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -15,6 +15,7 @@ public class PlayerStat
 {
     public StatsChangeType statsChangeType;
     [Range(1, 100)] public float maxHealth;
+    [Range(1, 100)] public float currentHealth;
     [Range(1f, 20f)] public float speed;
     public AttackSO attackSO;
 }
